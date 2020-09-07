17d0dd6ea4bca9929f82ab39bd5ff5559181dc8f
David Oliveira <luiz.david@cencosud.com.br>
2020-09-07 20:22:14 -0300

    Versão 1.00

---
+++ src/DevIO.App/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DevIO.App
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/DevIO.Bussines/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DevIO.Bussines
+{
+    public class Class1
+    {
+    }
+}
